e201935db13f70d2d76e361ec71357e921e6e946
ecaleb97 <valley_rob.0c@icloud.com>
2024-12-10 22:23:44 +0100

    Add getBooks function to filter books by search, genre, and pagination options

---
+++ src/services/book.ts
@@ -0,0 +1,32 @@
+import type { BookFilters } from "@/types";
+import book from "../../books.json";
+
+export function getBooks(options: BookFilters) {
+	let filteredBooks = book.library;
+
+	if (options?.search) {
+		filteredBooks = filteredBooks.filter((book) => {
+			return book.book.title
+				.toLowerCase()
+				.includes(options.search?.toLowerCase() || "");
+		});
+	}
+
+	if (options?.genre) {
+		filteredBooks = filteredBooks.filter((book) => {
+			return book.book.genre === options.genre;
+		});
+	}
+
+	if (options.genre === "All") {
+		filteredBooks = book.library;
+	}
+
+	if (options.page) {
+		filteredBooks = filteredBooks.filter((book) => {
+			return book.book.pages <= (options.page as number);
+		});
+	}
+
+	return filteredBooks;
+}
