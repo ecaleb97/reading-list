018e4d22044437d751582e5a3d78af613eecfc41
ecaleb97 <valley_rob.0c@icloud.com>
2024-12-21 19:47:13 +0100

    Refactor useBookFilters hook to use nuqs for state management and simplify filter handling

--- src/hooks/use-book-filters.ts
+++ src/hooks/use-book-filters.ts
@@ -1,43 +1,36 @@
 import type { BookFilters } from "@/types";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useQueryState, useQueryStates } from "nuqs";
 import { useCallback } from "react";
 
-export function useBookFilters() {
-	const searchParams = useSearchParams();
-	const { replace } = useRouter();
-	const pathname = usePathname();
+export function useBookParams() {
+	const [search, setSearch] = useQueryState<BookFilters["search"]>("search", {
+		defaultValue: "",
+		history: "push",
+		parse: (value) => value as BookFilters["search"],
+	});
 
-	const search = searchParams.get("search") as BookFilters["search"];
-	const genre = searchParams.get("genre") as BookFilters["genre"];
-	const page = searchParams.get("page")
-		? Number.parseInt(searchParams.get("page") as string)
-		: undefined;
+	const [genre, setGenre] = useQueryState<BookFilters["genre"]>("genre", {
+		history: "push",
+		defaultValue: "Ciencia ficción",
+		parse: (value) => value as BookFilters["genre"],
+	});
 
 	const setFilters = useCallback(
 		(filters: BookFilters) => {
-			const params = new URLSearchParams(searchParams.toString());
 			if (filters.search !== undefined) {
-				params.set("search", filters.search);
+				setSearch(filters.search);
 			}
-
 			if (filters.genre) {
-				params.set("genre", filters.genre);
+				setGenre(filters.genre);
 			}
-			if (filters.page) {
-				params.set("page", filters.page.toString());
-			}
-
-			replace(`${pathname}?${params.toString()}`);
-
-			return params.toString();
 		},
-		[searchParams, pathname, replace],
+		[setGenre, setSearch],
 	);
 
 	return {
 		search,
 		genre,
-		page,
 		setFilters,
 	};
 }
