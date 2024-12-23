18d46f40e7a0c9329ae950f97e6019a83cffff6c
ecaleb97 <valley_rob.0c@icloud.com>
2024-12-23 23:12:34 +0100

    Add function to fetch all books with error handling

--- src/services/book.ts
+++ src/services/book.ts
@@ -1,11 +1,23 @@
 import { client } from "@/lib/rpc";
 import type { BookFilters } from "@/types";
 
+export async function getBooks() {
+	const response = await client.api.books.all.$get();
+
+	if (!response.ok) {
+		throw new Error(`Error fetching books: ${response.statusText}`);
+	}
+
+	const data = await response.json();
+
+	return data;
+}
+
 export async function getBooksByFilters({ search, genre }: BookFilters) {
 	const response = await client.api.books.$get({
 		query: {
 			search: search as string,
-			genre: genre as string, 
+			genre: genre as string,
 		},
 	});
 
