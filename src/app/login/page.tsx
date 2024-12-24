7762dbcff1bd40eb49a38610c901164e8e043d8d
ecaleb97 <valley_rob.0c@icloud.com>
2024-12-24 10:33:22 +0100

    Remove BookPage, MyBooksPage, and LoginPage components to streamline the application

--- src/app/login/page.tsx
+++
@@ -1,4 +0,0 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription } from "@/components/ui/card";
-import { CardHeader } from "@/components/ui/card";
-import { CardTitle } from "@/components/ui/card";
